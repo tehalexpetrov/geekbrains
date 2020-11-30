8975fd50b3abed8bc1028090c0044fb8fa58885c
Alexander <tehalexpetrov@gmail.com>
2020-12-01 00:24:19 +0300

    Задание 1

--- java/src/lesson1/main.java
+++ java/src/lesson1/main.java
@@ -1,4 +1,18 @@
 package lesson1;
 
 public class main {
-}
+    public static void main(String[] args) {
+        variableTest();
+    }
+
+
+    private static void variableTest() {
+        int a = 5;
+        double b = 25.0;
+        long c = 50L;
+        byte d = 100;
+
+        int result = (int) (a * (b + (c / d)));
+        System.out.println(result);
+    }
+    }
