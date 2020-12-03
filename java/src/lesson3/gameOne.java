e0b6bd226a2ca642c20047a3c43581c8b798d24d
Alexander <tehalexpetrov@gmail.com>
2020-12-03 19:26:14 +0300

    Урок 3. Задание 1 готово

--- java/src/lesson3/gameOne.java
+++ java/src/lesson3/gameOne.java
@@ -12,13 +12,16 @@ public class gameOne {
 
 
         while (true) {
-            System.out.println("Программа загадала число от 1 до 10");
+            System.out.println("Программа загадала число от 0 до 9");
             System.out.println("Попробуйте угадать число. У Вас 3 попытки");
 
-            int multiple = (int) Math.ceil(Math.random() * 9);
+            int num1 = 0;
+            int num2 = 9;
+
+            int multiple = num1 + (int) Math.round(Math.random() * num2);
             System.out.println(multiple);
 
-            System.out.println("Введите число от 1 до 10: ");
+            System.out.println("Введите число от 0 до 9: ");
 
 
         int maxCount = 3;
@@ -26,42 +29,34 @@ public class gameOne {
 
         for (int i = 0; i < maxCount; i++) {
             count++;
+
             int userAnswer = scanner.nextInt();
 
             if (userAnswer == multiple) {
                 System.out.println("Поздравляю! Вы угадали число.");
                 break;
-
             } else if (count >= maxCount) {
-
                 System.out.println("К сожалению, Вы проиграли. Правильный ответ: " + multiple + "\n");
-
             } else if (userAnswer < multiple) {
-
                 System.out.println("Число меньше ответа");
-
             } else if (userAnswer > multiple) {
-
                 System.out.println("Число больше ответа");
             }
-
             System.out.println("У Вас осталось " + (maxCount - count) + " попыток");
         }
-
             System.out.println("Хотите сыграть еще раз?. Нажмите 1 - да или 0 - нет");
 
-        switch (in.nextInt()){
-            case 1 :
-                continue;
-            case  0 :
-                doExit();
-            default:
-                System.out.println("Попробуйте еще раз");
-        }
+            switch (in.nextInt()){
+                case 1 :
+                    break;
+                case  0 :
+                    doExit();
+                default:
+                    System.out.println("ведите 1 или 0");
+           }
 
         }
     }
-
 
     private static void doExit() {
         System.exit(0);
