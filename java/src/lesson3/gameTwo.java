7d9375e975422a7386577da6ce2039d1776bf960
Alexander <tehalexpetrov@gmail.com>
2020-12-03 19:26:50 +0300

    Урок 3. Задание 2 начало

---
+++ java/src/lesson3/gameTwo.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class gameTwo {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String [] words ={"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+    }
+
+}
